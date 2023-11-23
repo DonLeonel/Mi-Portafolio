40dfa49fde9257197b14677dec2c5a8ff12079cb
DonLeonel <leonel.don@hotmail.es>
2023-11-23 18:52:49 -0300

    corrigiendo Cv-pdf Arreglo

--- src/component/FooterComp.jsx
+++ src/component/FooterComp.jsx
@@ -7,7 +7,7 @@ export const FooterComp = () => {
     <footer>
       <div className='opciones-footer'>
         <a href='#' className='button-cv' aria-label='volver hacia arriba'>Ir arriba</a>
-        <a download='Cv-Leonel-don.pdf' aria-label='descargar currículum' href={cv} className='button-cv'>Descargar <br /> Curriculum</a>
+        <a download='Cv-LeonelDon.pdf' aria-label='descargar currículum' href={cv} className='button-cv'>Descargar <br /> Currículum</a>
       </div>
       <div className='copirigth'>
         <span><i className="fa-regular fa-copyright"></i><br />Copyright</span>
